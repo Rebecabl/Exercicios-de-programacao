d4799b90ead5242d80da82251a386bfc906ff1fe
Rebeca Barbosa <107273033+Rebecabl@users.noreply.github.com>
2023-04-11 20:36:54 -0300

    calculo do IMC
    Este é um exercício básico de programação em Python para relembrar conceitos de precedência e operadores aritméticos. A precedência determina a ordem em que as operações devem ser executadas em uma expressão matemática.
    
    Os operadores aritméticos são os símbolos usados para realizar operações matemáticas em Python, como adição (+), subtração (-), multiplicação (*), divisão (/) e exponenciação (**). Além disso, Python também oferece operadores para o resto da divisão (%), divisão inteira (//) e negação (-) de números.

--- calculoDoIMC.py
+++ calculoDoIMC.py
@@ -1,8 +1,11 @@
 nome = "Maria Antonia"
 alturaa = 1.54
 peso = 54
-imc + peso / altura ** 2
+imc = peso / altura ** 2
 
 print(nome, "tem", altura, "de altura", )
 print("pesa", peso, "quilos e seu imc é", )
 print(imc)
+
+# Este é um exercício de programação básica em Python, que tem como objetivo ajudar a relembrar os conceitos de precedência e operadores aritméticos. Em programação, a precedência é a ordem em que as operações são executadas em uma expressão matemática. É importante lembrar que certos operadores têm maior precedência do que outros, o que significa que eles serão executados antes.
+# Os operadores aritméticos básicos em Python são adição (+), subtração (-), multiplicação (*), divisão (/) e o operador de módulo (%). É possível utilizar esses operadores para realizar cálculos matemáticos simples, como somar dois números ou multiplicá-los.
